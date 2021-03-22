a5c3b767d519334296f13404090c7a19a9be899d
Cyril Gavala <cyril.gavala@dazn.com>
2021-03-22 10:27:53 +0100

    Minor updates

--- src/component/CustomChart.jsx
+++ src/component/CustomChart.jsx
@@ -1,87 +1,39 @@
-import {Spinner} from "react-bootstrap";
 import {CartesianGrid, Bar, Line, BarChart, LineChart, Tooltip, Legend, XAxis, YAxis, ResponsiveContainer} from 'recharts';
-import {useEffect, useState} from "react";
-import axios from "axios";
-
-const apiUrl = "https://covid-19-api.herokuapp.com/"
 
 export default function CustomChart(props) {
 
-  const [data, setData] = useState([])
-  const [isLoading, setLoading] = useState(true)
-
-  async function loadSeriesData(suffix, range) {
-
-    function calculateParams(range) {
-      const endDate = new Date(), startDate = new Date();
-      endDate.setUTCHours(0, 0, 0, 0);
-      startDate.setUTCHours(0, 0, 0, 0);
-      if ("all" === range) {
-        return {
-          startDate: new Date(2020, 10, 26).toISOString(),
-          endDate: endDate.toISOString()
-        }
-      } else {
-        startDate.setDate(startDate.getDate() - range);
-        return {
-          startDate: startDate.toISOString(),
-          endDate: endDate.toISOString()
-        }
-      }
-    }
-
-    setLoading(false)
-    return axios.get(apiUrl + 'series/' + suffix, {params: calculateParams(range)});
+  let counter = 0
+  let graph
+  let graphElements
+  const innerElements = []
+  innerElements.push(<XAxis dataKey={"date"} tick={{fill: 'white'}} tickFormatter={o => new Date(o).toLocaleDateString()}/>)
+  innerElements.push(<YAxis yAxisId={0} tick={{fill: 'white'}} type={"number"} domain={['auto', 'dataMax+30']} allowDataOverflow/>)
+  innerElements.push(<Tooltip isAnimationActive={false} cursor={{fill: 'transparent'}} labelFormatter={(o) => new Date(o).toLocaleDateString()}/>)
+  innerElements.push(<Legend/>)
+  innerElements.push(<CartesianGrid stroke="#fff"/>)
+  if ("bar" === props.type) {
+    graphElements = props.labels.map(i => {
+      return <Bar key={i.label + counter++} type="monotone" dataKey={i.label} fill={i.color} yAxisId={0}/>
+    })
+    graph = <BarChart width={window.innerWidth - 100} height={400} data={props.data} margin={{top: 0, right: 50, left: 20, bottom: 0}}>
+      {innerElements}
+      {graphElements}
+    </BarChart>
   }
-
-  useEffect(() => {
-    const loadData = async () => {
-      await loadSeriesData(props.querySuffix, props.range).then(res => setData(res.data))
-      return "Statistics of " + props.querySuffix + " loaded"
-    }
-    loadData().then(r => console.log(r))
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
-  if (!isLoading) {
-    let graph
-    let graphElements
-    const innerElements = []
-    innerElements.push(<XAxis dataKey={"date"} tick={{fill: 'white'}} tickFormatter={o => new Date(o).toLocaleDateString()}/>)
-    innerElements.push(<YAxis yAxisId={0} tick={{fill: 'white'}} type={"number"} domain={['auto', 'dataMax+30']} allowDataOverflow/>)
-    innerElements.push(<Tooltip isAnimationActive={false} cursor={{fill: 'transparent'}} labelFormatter={(o) => new Date(o).toLocaleDateString()}/>)
-    innerElements.push(<Legend/>)
-    innerElements.push(<CartesianGrid stroke="#fff"/>)
-    if ("bar" === props.type) {
-      graphElements = props.labels.map(i => {
-        return <Bar key={i.label} type="monotone" dataKey={i.label} fill={i.color} yAxisId={0}/>
-      })
-      graph = <BarChart width={window.innerWidth - 100} height={400} data={data}
-                        margin={{top: 0, right: 50, left: 20, bottom: 0}}>
-        {innerElements}
-        {graphElements}
-      </BarChart>
-    }
-    if ("line" === props.type) {
-      graphElements = props.labels.map(i => {
-        return <Line key={i.label} type="monotone" dataKey={i.label} strokeWidth={3} stroke={i.color} yAxisId={0}/>
-      })
-      graph = <LineChart width={window.innerWidth - 100} height={400} data={data}
-                         margin={{top: 0, right: 50, left: 20, bottom: 0}}>
-        {innerElements}
-        {graphElements}
-      </LineChart>
-    }
-    return <div className={"chart-wrapper"}>
-      <h3 className={"chart-title"}>{props.title}</h3>
-      <ResponsiveContainer>
-        {graph}
-      </ResponsiveContainer>
-    </div>
-  } else {
-    return <div className={"loading-wrapper"}>
-      <Spinner animation="border" role="status">
-        <span className="sr-only">Loading...</span>
-      </Spinner>
-    </div>
+  if ("line" === props.type) {
+    graphElements = props.labels.map(i => {
+      return <Line key={i.label + counter++} type="monotone" dataKey={i.label} strokeWidth={3} stroke={i.color} yAxisId={0}/>
+    })
+    graph = <LineChart width={window.innerWidth - 100} height={400} data={props.data}
+                       margin={{top: 0, right: 50, left: 20, bottom: 0}}>
+      {innerElements}
+      {graphElements}
+    </LineChart>
   }
+  return <div className={"chart-wrapper"}>
+    <h3 className={"chart-title"}>{props.title}</h3>
+    <ResponsiveContainer>
+      {graph}
+    </ResponsiveContainer>
+  </div>
 }
--- src/containers/Graphs.jsx
+++ src/containers/Graphs.jsx
@@ -1,31 +1,88 @@
-import {Container, Dropdown} from 'react-bootstrap';
+import {Container, Dropdown, Spinner} from 'react-bootstrap';
 import CustomChart from "../component/CustomChart";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const apiUrl = "https://covid-19-api.herokuapp.com/"
 
 export default function Graphs() {
 
   const [range, setRange] = useState("all")
+  const [posPercentage, setPosPercentage] = useState([])
+  const [deaths, setDeaths] = useState([])
+  const [dailyTests, setDailyTests] = useState([])
+  const [dailyDeaths, setDailyDeaths] = useState([])
+  const [isLoading, setLoading] = useState(true)
 
+  useEffect(() => {
+    loadData(range).then(r => console.log(r))
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-  return <Container fluid id="graphs-container">
-    <Dropdown onSelect={value => setRange(value)} drop={"right"}>
-      <Dropdown.Toggle variant="secondary" id="select-date-range">
-        Select date range (Current: {"all" === range ? "All)" : "Last " + range + " days)"}
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
-        <Dropdown.Item eventKey={"all"}>All</Dropdown.Item>
-        <Dropdown.Item eventKey={90}>Last 90 days</Dropdown.Item>
-        <Dropdown.Item eventKey={30}>Last 30 days</Dropdown.Item>
-        <Dropdown.Item eventKey={7}>Last 7 days</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-    <CustomChart key={"pp_" + range} type={"line"} title={"Positive percentage"} querySuffix={"positivePercentage"}
-                 labels={[{label: "percentage", color: "cyan"}]} range={range}/>
-    <CustomChart key={"dt_" + range} type={"bar"} title={"Daily tests"} querySuffix={"testsDaily"} range={range}
-                 labels={[{label: "tests", color: "cyan"}, {label: "confirmed", color: "lightblue"}]}/>
-    <CustomChart key={"d_" + range} type={"line"} title={"Deaths"} querySuffix={"deaths"} labels={[{label: "deaths", color: "cyan"}]}
-                 range={range}/>
-    <CustomChart key={"dd_" + range} type={"bar"} title={"Deaths daily"} querySuffix={"deathsDaily"}
-                 labels={[{label: "deaths", color: "cyan"}]} range={range}/>
-  </Container>
+  async function reloadData(selectedRange) {
+    setRange(selectedRange)
+    setLoading(true)
+    loadData(selectedRange).then(r => console.log(r))
+  }
+
+  async function loadData(range) {
+    const params = calculateParams(range)
+    await loadSeriesData("positivePercentage", params).then(res => setPosPercentage(res.data))
+    await loadSeriesData("testsDaily", params).then(res => setDailyTests(res.data))
+    await loadSeriesData("deaths", params).then(res => setDeaths(res.data))
+    await loadSeriesData("deathsDaily", params).then(res => setDailyDeaths(res.data))
+    setLoading(false)
+    return "Statistical data loaded"
+  }
+
+  async function loadSeriesData(suffix, params) {
+    return axios.get(apiUrl + 'series/' + suffix, {params: params});
+  }
+
+  function calculateParams(range) {
+    const endDate = new Date(), startDate = new Date();
+    endDate.setUTCHours(0, 0, 0, 0);
+    startDate.setUTCHours(0, 0, 0, 0);
+    if ("all" === range) {
+      return {
+        startDate: new Date(2020, 10, 26).toISOString(),
+        endDate: endDate.toISOString()
+      }
+    } else {
+      startDate.setDate(startDate.getDate() - range);
+      return {
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString()
+      }
+    }
+  }
+
+  if (!isLoading) {
+    return <Container fluid id="graphs-container">
+      <Dropdown onSelect={value => reloadData(value)} drop={"right"}>
+        <Dropdown.Toggle variant="secondary" id="select-date-range">
+          Select date range (Current: {"all" === range ? "All)" : "Last " + range + " days)"}
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item eventKey={"all"}>All</Dropdown.Item>
+          <Dropdown.Item eventKey={90}>Last 90 days</Dropdown.Item>
+          <Dropdown.Item eventKey={30}>Last 30 days</Dropdown.Item>
+          <Dropdown.Item eventKey={7}>Last 7 days</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <CustomChart key={"pp_" + range} type={"line"} title={"Positive percentage"}
+                   labels={[{label: "percentage", color: "cyan"}]} data={posPercentage}/>
+      <CustomChart key={"dt_" + range} type={"bar"} title={"Daily tests"} data={dailyTests}
+                   labels={[{label: "tests", color: "cyan"}, {label: "confirmed", color: "lightblue"}]}/>
+      <CustomChart key={"d_" + range} type={"line"} title={"Deaths"} labels={[{label: "deaths", color: "cyan"}]}
+                   data={deaths}/>
+      <CustomChart key={"dd_" + range} type={"bar"} title={"Deaths daily"} labels={[{label: "deaths", color: "cyan"}]}
+                   data={dailyDeaths}/>
+    </Container>
+  } else {
+    return <div className={"loading-wrapper"}>
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    </div>
+  }
 }
