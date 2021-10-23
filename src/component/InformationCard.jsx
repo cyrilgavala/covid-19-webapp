f1bffb67fa1da08ba93702d011e550c2a42a45a9
Cyril Gavala <CGavala@gohealth.com>
2021-10-23 12:36:02 +0200

    ----- New version 6.0.1 ----- - minor styling update

--- src/component/InformationCard.jsx
+++ src/component/InformationCard.jsx
@@ -2,11 +2,11 @@ import {Card} from 'react-bootstrap';
 
 export default function InformationCard(props) {
 
-  return <Card className={"col-sm-8 col-md-4 col-lg-4 col-xl-3"}>
+  return <Card>
     <Card.Header >{props.label}</Card.Header>
     <Card.Body>
       <Card.Text>
-        {props.data} ({props.delta > 0 ? " +" + props.delta : props.delta} )
+        {props.data} ({props.delta > 0 ? " +" + props.delta : props.delta})
       </Card.Text>
     </Card.Body>
   </Card>
