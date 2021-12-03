2cf2484b6048ad626a21de33abfbe998a32275d9
Cyril Gavala <cyril.gavala@gmail.com>
2021-12-03 21:45:11 +0100

    ----- New version 6.1.3 ----- - styling update

--- src/component/CustomLineChart.jsx
+++ src/component/CustomLineChart.jsx
@@ -8,13 +8,13 @@ export default function CustomLineChart(props) {
     })
     const yAxis = props.labels.map((item, index) => {
         return <YAxis key={index % 2 === 0 ? "left" : "right"} yAxisId={index % 2 === 0 ? "left" : "right"} tick={{fill: item.color}} width={30}
-                      type={"number"} domain={['auto', 'dataMax+25']} orientation={index % 2 === 0 ? "left" : "right"} scale={"auto"}/>
+                      type={"number"} domain={['auto', 'dataMax+30']} orientation={index % 2 === 0 ? "left" : "right"} scale={"auto"}/>
     })
 
     return <div className={"chart-wrapper"}>
         <div className={"chart-title"}>{props.title}</div>
         <ResponsiveContainer minWidth={800}>
-            <LineChart width={window.innerWidth} height={330} data={props.data} margin={{top: 0, right: 30, left: 30, bottom: 0}}>
+            <LineChart width={window.innerWidth} height={400} data={props.data} margin={{top: 20, right: 50, left: 50, bottom: 20}}>
                 <XAxis dataKey={"date"} tick={{fill: '#101726'}} tickFormatter={o => new Date(o).toLocaleDateString()} height={30}/>
                 <Tooltip isAnimationActive={false} cursor={{fill: 'transparent'}} labelFormatter={(o) => new Date(o).toLocaleDateString()}/>
                 <Legend height={30} verticalAlign={"top"} iconType={"plainline"}/>
