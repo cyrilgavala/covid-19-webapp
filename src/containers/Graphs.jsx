e91848f9c2304dd38d45e0490fcde92d2bd79734
Cyril Gavala <cyril.gavala@gmail.com>
2021-12-04 20:58:09 +0100

    ----- New version 7.0.0 ----- - reworked date range approach - styling update - finally fixed responsiveness

--- src/containers/Graphs.jsx
+++ src/containers/Graphs.jsx
@@ -1,4 +1,3 @@
-import {Dropdown} from 'react-bootstrap';
 import CustomLineChart from "../component/CustomLineChart";
 import {useEffect, useState} from "react";
 import axios from "axios";
@@ -7,28 +6,27 @@ import Spinner from "../component/Spinner";
 
 export default function Graphs() {
 
-    const [range, setRange] = useState("all")
+    const [startDate, setStartDate] = useState("2020-11-27")
+    const [endDate, setEndDate] = useState(new Date().toISOString().slice(0, 10))
     const [posPercentage, setPosPercentage] = useState([])
     const [deathsData, setDeathsData] = useState([])
     const [dailyTests, setDailyTests] = useState([])
     const [isLoading, setLoading] = useState(true)
 
     useEffect(() => {
-        loadData(range).then(r => console.log(r))
+        loadData(startDate, endDate).then(r => console.log(r))
     }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-    async function reloadData(selectedRange) {
-        setRange(selectedRange)
-        setLoading(true)
-        loadData(selectedRange).then(r => console.log(r))
-    }
-
-    async function loadData(range) {
-        const params = calculateParams(range)
+    const loadData = async (start, end) => {
+        const params = translateDatesToParams(start, end)
         await loadSeriesData("positivePercentage", params).then(res => setPosPercentage(res.data))
         await loadSeriesData("testsDaily", params).then(res => setDailyTests(res.data))
-        const deathsTotal = await loadSeriesData("deaths", params).then(res => {return res.data})
-        const deathsDaily = await loadSeriesData("deathsDaily", params).then(res => {return res.data})
+        const deathsTotal = await loadSeriesData("deaths", params).then(res => {
+            return res.data
+        })
+        const deathsDaily = await loadSeriesData("deathsDaily", params).then(res => {
+            return res.data
+        })
 
         setDeathsData(deathsDaily.map((item, index) => {
             return {deathsDaily: item.deaths, deathsTotal: deathsTotal[index].deaths, date: item.date}
@@ -38,44 +36,55 @@ export default function Graphs() {
         return "Statistical data loaded"
     }
 
-    async function loadSeriesData(suffix, params) {
+    const loadSeriesData = async (suffix, params) => {
         return axios.get(properties.apiUrl + 'series/' + suffix, {params: params});
     }
 
-    function calculateParams(range) {
-        if ("all" === range) {
-            return {}
-        } else {
-            const startDate = new Date();
-            startDate.setUTCHours(0, 0, 0, 0);
-            startDate.setDate(startDate.getDate() - range);
-            return {
-                startDate: startDate.toISOString(),
-            }
+    const translateDatesToParams = (start, end) => {
+        return {
+            startDate: new Date(start).toISOString(),
+            endDate: new Date(end).toISOString()
         }
+    }
+
+    const handleStartDateSelect = async (event) => {
+        event.preventDefault()
+        setStartDate(event.target.value)
+        setLoading(true)
+        await loadData(event.target.value, endDate)
+    }
+
+    const handleEndDateSelect = async (event) => {
+        event.preventDefault()
+        setEndDate(event.target.value)
+        setLoading(true)
+        await loadData(startDate, event.target.value)
     }
 
     if (!isLoading) {
         return <div id="graphs-container">
-            <Dropdown id={"date-range-dropdown"} onSelect={value => reloadData(value)} drop={"down"}>
-                <Dropdown.Toggle variant="secondary" id="select-date-range">
-                    {"Select range"}
-                </Dropdown.Toggle>
-                <Dropdown.Menu id={"date-range-dropdown-menu"}>
-                    <Dropdown.Item eventKey={"all"}>All</Dropdown.Item>
-                    <Dropdown.Item eventKey={365}>Last year</Dropdown.Item>
-                    <Dropdown.Item eventKey={180}>Last half year</Dropdown.Item>
-                    <Dropdown.Item eventKey={90}>Last 3 months</Dropdown.Item>
-                    <Dropdown.Item eventKey={30}>Last month</Dropdown.Item>
-                    <Dropdown.Item eventKey={7}>Last week</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-            <CustomLineChart syncId="1" key={"pp_" + range} title={"Positive percentage"} labels={[{label: "percentage", color: "#4c6353"}]}
+            <div id={"date-inputs-wrapper"}>
+                <div className={"input-wrapper"}>
+                    <label className={"label-input"}>Start date: </label>
+                    <input className={"date-input"} type={"date"} min={"2020-11-27"} max={new Date().toDateString()}
+                           value={startDate} onChange={handleStartDateSelect}/>
+                </div>
+                <div className={"input-wrapper"}>
+                    <label className={"label-input"}>End date: </label>
+                    <input className={"date-input"} type={"date"} min={"2020-11-27"} max={new Date().toDateString()}
+                           value={endDate} onChange={handleEndDateSelect}/>
+                </div>
+            </div>
+            <CustomLineChart key={"pp_" + startDate + endDate} title={"Positive percentage"}
+                             labels={[{label: "percentage", color: "#4c6353"}]}
                              data={posPercentage}/>
-            <CustomLineChart syncId="1" key={"dt_" + range} title={"Daily tests"} data={dailyTests}
+            <CustomLineChart key={"dt_" + startDate + endDate} title={"Daily tests"} data={dailyTests}
                              labels={[{label: "tests", color: "#4c6353"}, {label: "confirmed", color: "#73303c"}]}/>
-            <CustomLineChart key={"d_" + range} title={"Deaths"}
-                             labels={[{label: "deathsDaily", color: "#4c6353"}, {label: "deathsTotal", color: "#73303c"}]}
+            <CustomLineChart key={"d_" + startDate + endDate} title={"Deaths"}
+                             labels={[{label: "deathsDaily", color: "#4c6353"}, {
+                                 label: "deathsTotal",
+                                 color: "#73303c"
+                             }]}
                              data={deathsData}/>
         </div>
     } else {
